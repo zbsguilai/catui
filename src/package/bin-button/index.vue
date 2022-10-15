b8cac0025f428b484a6326d20dad6bec41b7b2aa
zhangbingshuai <1533644421@qq.com>
2022-10-15 23:28:41 +0800

    update code 优化

---
+++ src/package/bin-button/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <button @click="handleClick" :disabled='disabled'>
+        <slot></slot>
+    </button>
+</template>
+<script>
+export default {
+    name: "BinButton",
+    props: {
+        disabled: Boolean
+    },
+    methods: {
+        handleClick(evt) {
+            this.$emit('click', evt);
+        }
+    }
+};
+</script>
---
+++ src/package/index.js
@@ -0,0 +1,11 @@
+import BinButton from "../package/bin-button/index.vue"; // 引入封装好的组件
+const coms = [PigButton]; // 将来如果有其它组件,都可以写到这个数组里
+
+// 批量组件注册
+const install = function (Vue) {
+  coms.forEach((com) => {
+    Vue.component(com.name, com);
+  });
+};
+
+export default install; // 这个方法以后再使用的时候可以被use调用

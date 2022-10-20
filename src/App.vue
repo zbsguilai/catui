651c547a87b8ef41741035cfeb5aa65181848055
zhangbingshuai <1533644421@qq.com>
2022-10-20 11:33:28 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <bin-button disabled>按钮</bin-button>
+    <!-- <bin-button disabled>按钮</bin-button> -->
     <!-- <bin-button>按钮</bin-button> -->
   </div>
 </template>
